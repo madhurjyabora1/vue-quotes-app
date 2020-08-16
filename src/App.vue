fe9aec9351dbf7061708c83d0ac4e80523ff63ef
madhurjyabora1 <mrbora10@gmail.com>
2020-08-16 14:23:23 +0530

    bug fixes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="container">
+  <div class="container">
     <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     <app-new-quote @quoteAdded='newQuote'></app-new-quote>
     <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
